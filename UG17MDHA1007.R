b36651942a7937b5cc591d74b6b3df3b1c8afc94
Bkay77 <94083458+Bkay77@users.noreply.github.com>
2024-02-17 09:47:10 +0100

    Update UG17MDHA1007.R

--- UG17MDHA1007.R
+++ UG17MDHA1007.R
@@ -1,8 +1,13 @@
-#Bioinformatics script
+# Bioinformatics script
 
+# Define variables
+name <- c("Nahum Joseph")
+Email <- c("Nahumjoseph2@gmail.com")
+Slack_Username <- c("@Nahum Joseph")
+Area_of_Interest <- c("Data mining")
 
-name <-c("Nahum Joseph")
-Email <-c("Nahumjoseph2@gmail.com")
-Slack Username<-c("@Nahum Joseph")
-Area of Interest<-c("Data mining")
-
+# Print the values
+cat("Name:", name, "\n")
+cat("Email:", Email, "\n")
+cat("Slack Username:", Slack_Username, "\n")
+cat("Area of Interest:", Area_of_Interest, "\n")
